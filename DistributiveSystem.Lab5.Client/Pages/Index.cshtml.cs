3421bb52d3e25681a1fab6cad8f54238f1e70a57
hak.dok0@gmail.com <hak.dok0@gmail.com>
2023-02-17 14:32:39 +0300

    Добавьте файлы проекта.

---
+++ DistributiveSystem.Lab5.Client/Pages/Index.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.IO;
+using System.Text;
+
+namespace DistributiveSystem.Lab5.Client.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public string Message { get; private set; } = "";
+     
+
+        static HttpClient httpClient = new HttpClient();
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            
+        }
+
+        public  void OnPost(int amount, string rate)
+        {
+
+                float resultRate;
+                if (float.TryParse(rate, out resultRate))
+                    Message = GetResult(amount, resultRate);
+                else
+                    Message = "Error parse rate field";
+
+        }
+
+        public string GetResult(int amount, float rate)
+        {
+            string url = $"https://localhost:7101/service?amount={amount}&rate={rate.ToString(System.Globalization.CultureInfo.InvariantCulture)}";
+            
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
+
+            var response = httpClient.Send(request);
+            var stream = response.Content.ReadAsStream();
+            stream.Position = 0;
+            string ans;
+            using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+            {
+                ans = reader.ReadToEnd();
+            }
+            return ans;
+        }
+    }
+}
---
+++ DistributiveSystem.Lab5/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/service", (int amount, float rate) =>
+{
+
+    float ans = amount * rate;
+    return ans;
+});
+
+app.Run();
+
